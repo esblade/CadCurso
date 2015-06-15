89684406c24ce7ea3031fbf85802cc7a0767d34e
rafaelnet29 <rafaelnet29@users.noreply.github.com>
2015-06-15 02:37:47 -0300

    - Modificações no relatorio - classe ordemServicoDP em testes - Banco reiniciado

---
+++ src/ConexaoAtributos/Setor.java
@@ -0,0 +1,26 @@
+package ConexaoAtributos;
+
+public class Setor {
+	private int cod_setor;
+	private String dsc_setor;
+	private String nom_setor;
+	
+	public int getCod_setor() {
+		return cod_setor;
+	}
+	public void setCod_setor(int cod_setor) {
+		this.cod_setor = cod_setor;
+	}
+	public String getDsc_setor() {
+		return dsc_setor;
+	}
+	public void setDsc_setor(String dsc_setor) {
+		this.dsc_setor = dsc_setor;
+	}
+	public String getNom_setor() {
+		return nom_setor;
+	}
+	public void setNom_setor(String nom_setor) {
+		this.nom_setor = nom_setor;
+	}
+}
